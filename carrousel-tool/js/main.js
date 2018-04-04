f2f710992af7563d8934481b03b19c049e64f373
MuzLin <1030996014@qq.com>
2018-04-04 16:24:06 +0800

    version 1.2 自动播放

--- carrousel-tool/js/main.js
+++ carrousel-tool/js/main.js
@@ -1,9 +1,31 @@
-$(function(){
+$(function () {
+    let n = 0;
     let btns = $('.button > button')
     for (let i = 0; i < btns.length; i++) {
-        $(btns[i]).on('click',function(e){
+        $(btns[i]).on('click', function (e) {
             let index = $(e.currentTarget).index()
-            $('.wapper').css('transform','translateX(-'+ index*100 +'%)')
+            $('.wapper').css({'transform' : 'translateX(-' + index * 100 + '%)'})
+            $(e.currentTarget).addClass('red').siblings('.red').removeClass('red')
+            n = index
         })
+    }
+
+    let btnLength = btns.length
+    let id = timer()
+    $('.window').on('mouseenter',function(){
+        window.clearInterval(id)
+    })
+
+    $('.window').on('mouseleave',function(){
+        id = timer()
+    })
+
+    function timer(){
+        let id = setInterval(() => {
+            btns.eq(n%btnLength).trigger('click')
+            btns.eq(n%btnLength).addClass('red').siblings('.red').removeClass('red')
+            n++
+        }, 1000)
+        return id;
     }
 })
