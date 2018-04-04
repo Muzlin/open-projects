f8b4ade150530295134b3bd530ccceb0dd35f791
MuzLin <1030996014@qq.com>
2018-04-04 16:33:52 +0800

    version 1.3 封装一些重复代码

--- carrousel-tool/js/main.js
+++ carrousel-tool/js/main.js
@@ -4,26 +4,31 @@ $(function () {
     for (let i = 0; i < btns.length; i++) {
         $(btns[i]).on('click', function (e) {
             let index = $(e.currentTarget).index()
-            $('.wapper').css({'transform' : 'translateX(-' + index * 100 + '%)'})
-            $(e.currentTarget).addClass('red').siblings('.red').removeClass('red')
+            $('.wapper').css({
+                'transform': 'translateX(-' + index * 100 + '%)'
+            })
+            activeBtn($(e.currentTarget))
             n = index
         })
     }
 
     let btnLength = btns.length
     let id = timer()
-    $('.window').on('mouseenter',function(){
+    $('.window').on('mouseenter', function () {
         window.clearInterval(id)
     })
 
-    $('.window').on('mouseleave',function(){
+    $('.window').on('mouseleave', function () {
         id = timer()
     })
 
-    function timer(){
+    function activeBtn($btn) {
+        $btn.addClass('red').siblings('.red').removeClass('red')
+    }
+
+    function timer() {
         let id = setInterval(() => {
-            btns.eq(n%btnLength).trigger('click')
-            btns.eq(n%btnLength).addClass('red').siblings('.red').removeClass('red')
+            btns.eq(n % btnLength).trigger('click')
             n++
         }, 1000)
         return id;
