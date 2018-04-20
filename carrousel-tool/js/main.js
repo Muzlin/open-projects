bbcc20f3ab823b82d5135f21e4324b8f4c39e697
MuzLin <1030996014@qq.com>
2018-04-20 13:59:26 +0800

    version 2.0 无缝轮播 实现过程

--- carrousel-tool/js/main.js
+++ carrousel-tool/js/main.js
@@ -1,42 +1,34 @@
-$(function () {
-    let n = 0;  // 当前播放的图片索引
-    let btns = $('.button > button')
-    for (let i = 0; i < btns.length; i++) {
-        $(btns[i]).on('click', function (e) {
-            let index = $(e.currentTarget).index()
-            $('.wapper').css({
-                'transform': 'translateX(-' + index * 100 + '%)'
-            })
-            activeBtn($(e.currentTarget))
-            n = index
+$(function(){
+    setTimeout(() => {
+        $('.images > img:nth-child(1)').css('transform','translateX(-100%)')
+        $('.images > img:nth-child(2)').css('transform','translateX(-100%)')
+        // 监听动画结束 初始化图片1的位置
+        $('.images > img:nth-child(1)').one('transitionend',function(e){
+            $(e.currentTarget).addClass('right').css('transform','none')
         })
-    }
-
-    let id = timer()
-    $('.window').on('mouseenter', function () {
-        window.clearInterval(id)
-    })
-
-    $('.window').on('mouseleave', function () {
-        id = timer()
-    })
-
-    let btnLength = btns.length // 按钮、图片的数量
-    /* 触发按钮状态 */
-    function activeBtn($btn) {
-        $btn.addClass('active').siblings('.active').removeClass('active')
-    }
-
-    /* 定时器 
-        当前的图片索引跟总数量取余
-        保证n%btnLength的范围在btnLength内
-        e.g. 0%3-0 1%3->1
-    */
-    function timer() {
-        let id = setInterval(() => {
-            btns.eq(n % btnLength).trigger('click')
-            n++
-        }, 1000)
-        return id;
-    }
+    }, 3000);
+    setTimeout(() => {
+        $('.images > img:nth-child(2)').css('transform','translateX(-200%)')
+        $('.images > img:nth-child(3)').css('transform','translateX(-100%)')
+        // 监听动画结束 初始化图片2的位置
+        $('.images > img:nth-child(2)').one('transitionend',function(e){
+            $(e.currentTarget).addClass('right').css('transform','none')
+        })
+    }, 6000);
+    setTimeout(() => {
+        $('.images > img:nth-child(1)').css('transform','translateX(-100%)')
+        $('.images > img:nth-child(3)').css('transform','translateX(-200%)')
+        // 监听动画结束 初始化图片2的位置
+        $('.images > img:nth-child(3)').one('transitionend',function(e){
+            $(e.currentTarget).addClass('right').css('transform','none')
+        })
+    }, 9000);
+    setTimeout(() => {
+        $('.images > img:nth-child(1)').css('transform','translateX(-200%)')
+        $('.images > img:nth-child(2)').css('transform','translateX(-100%)')
+        // 监听动画结束 初始化图片2的位置
+        $('.images > img:nth-child(1)').one('transitionend',function(e){
+            $(e.currentTarget).addClass('right').css('transform','none')
+        })
+    }, 12000);
 })
