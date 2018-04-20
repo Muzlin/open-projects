2c67655afb27e1d8bce3b10637651bc1a7a4c1d9
MuzLin <1030996014@qq.com>
2018-04-20 17:25:42 +0800

    version 2.2 自动播放

--- carrousel-tool/js/main.js
+++ carrousel-tool/js/main.js
@@ -1,27 +1,28 @@
-$(function(){
+$(function () {
     // 初始化图片位置
     $('.images > img:nth-child(1)').addClass('current')
     $('.images > img:nth-child(2)').addClass('enter')
+    $('.images > img:nth-child(3)').addClass('enter')
 
-    setTimeout(() => {
-        $('.images > img:nth-child(1)').addClass('leave').removeClass('current').one('transitionend',function(e){
-          // 监听动画完成后 将图片放入移入区
-          $('.images > img:nth-child(1)').removeClass('leave').addClass('enter')  
+    let n = 1;
+    setInterval(() => {
+        console.log(n)
+
+        $(`.images > img:nth-child(${x(n)})`).addClass('leave').removeClass('current').one('transitionend', function (e) {
+            // 监听动画完成后 将图片放入移入区
+            $(e.currentTarget).removeClass('leave').addClass('enter')
         })
-        $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
-    }, 3000);
-    setTimeout(() => {
-        $('.images > img:nth-child(2)').addClass('leave').removeClass('current').one('transitionend',function(e){
-          // 监听动画完成后 将图片放入移入区
-          $('.images > img:nth-child(2)').removeClass('leave').addClass('enter')  
-        })
-        $('.images > img:nth-child(3)').removeClass('enter').addClass('current')
-    }, 6000);
-    setTimeout(() => {
-        $('.images > img:nth-child(3)').removeClass('current').addClass('leave').one('transitionend',function(e){
-          // 监听动画完成后 将图片放入移入区
-          $('.images > img:nth-child(3)').removeClass('leave').addClass('enter')  
-        })
-        $('.images > img:nth-child(1)').removeClass('enter').addClass('current')
-    }, 9000);
+        $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+        n += 1
+    }, 3000)
 })
+
+function x(num) {
+    if (num > 3) {
+        num = num % 3
+        if (num === 0) {
+            num = 3
+        }
+    }
+    return num;1
+}
