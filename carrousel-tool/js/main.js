186f288a4ca6bb7c9b17f23a83bf980b762be72d
MuzLin <1030996014@qq.com>
2018-04-20 15:43:57 +0800

    version 2.1 样式分离

--- carrousel-tool/js/main.js
+++ carrousel-tool/js/main.js
@@ -1,34 +1,27 @@
 $(function(){
+    // 初始化图片位置
+    $('.images > img:nth-child(1)').addClass('current')
+    $('.images > img:nth-child(2)').addClass('enter')
+
     setTimeout(() => {
-        $('.images > img:nth-child(1)').css('transform','translateX(-100%)')
-        $('.images > img:nth-child(2)').css('transform','translateX(-100%)')
-        // 监听动画结束 初始化图片1的位置
-        $('.images > img:nth-child(1)').one('transitionend',function(e){
-            $(e.currentTarget).addClass('right').css('transform','none')
+        $('.images > img:nth-child(1)').addClass('leave').removeClass('current').one('transitionend',function(e){
+          // 监听动画完成后 将图片放入移入区
+          $('.images > img:nth-child(1)').removeClass('leave').addClass('enter')  
         })
+        $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
     }, 3000);
     setTimeout(() => {
-        $('.images > img:nth-child(2)').css('transform','translateX(-200%)')
-        $('.images > img:nth-child(3)').css('transform','translateX(-100%)')
-        // 监听动画结束 初始化图片2的位置
-        $('.images > img:nth-child(2)').one('transitionend',function(e){
-            $(e.currentTarget).addClass('right').css('transform','none')
+        $('.images > img:nth-child(2)').addClass('leave').removeClass('current').one('transitionend',function(e){
+          // 监听动画完成后 将图片放入移入区
+          $('.images > img:nth-child(2)').removeClass('leave').addClass('enter')  
         })
+        $('.images > img:nth-child(3)').removeClass('enter').addClass('current')
     }, 6000);
     setTimeout(() => {
-        $('.images > img:nth-child(1)').css('transform','translateX(-100%)')
-        $('.images > img:nth-child(3)').css('transform','translateX(-200%)')
-        // 监听动画结束 初始化图片2的位置
-        $('.images > img:nth-child(3)').one('transitionend',function(e){
-            $(e.currentTarget).addClass('right').css('transform','none')
+        $('.images > img:nth-child(3)').removeClass('current').addClass('leave').one('transitionend',function(e){
+          // 监听动画完成后 将图片放入移入区
+          $('.images > img:nth-child(3)').removeClass('leave').addClass('enter')  
         })
+        $('.images > img:nth-child(1)').removeClass('enter').addClass('current')
     }, 9000);
-    setTimeout(() => {
-        $('.images > img:nth-child(1)').css('transform','translateX(-200%)')
-        $('.images > img:nth-child(2)').css('transform','translateX(-100%)')
-        // 监听动画结束 初始化图片2的位置
-        $('.images > img:nth-child(1)').one('transitionend',function(e){
-            $(e.currentTarget).addClass('right').css('transform','none')
-        })
-    }, 12000);
 })
